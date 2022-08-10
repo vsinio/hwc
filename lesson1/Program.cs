d15d0d0022f433b7682fa517ad74be7a23ff3f66
Ivan <markelovv1999@yandex.ru>
2022-08-10 11:03:39 +0300

    add gitignore

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
