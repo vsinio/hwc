eb62257b01ce5702b5b2467857a6b2c55a9891d4
Ivan <markelovv1999@yandex.ru>
2022-08-10 12:55:19 +0300

    do hwnumber4

---
+++ lesson1/hwnumber4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число А сравнения:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число Б сравнения:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число С сравнения:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > b) max = a;
+if (b > a) max = b;
+if (c > a) max = c;
+if (c > b) max = c;
+Console.Write(max);
+
+
+
