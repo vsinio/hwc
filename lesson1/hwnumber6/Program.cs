962bf71783414c0fac725aa099ffe980274b78ab
Ivan <markelovv1999@yandex.ru>
2022-08-10 16:21:24 +0300

    fix code

--- lesson1/hwnumber6/Program.cs
+++ lesson1/hwnumber6/Program.cs
@@ -1,7 +1,6 @@
 ﻿Console.Write("Введите число для определения четных чисел: ");
 int a = Convert.ToInt32(Console.ReadLine());
-int n = 0;
-while 
-    Console.Write("нет");
+if (a % 2 != 0)
+    Console.Write("нет, нечет");
 else
-    Console.Write("да"); 
+    Console.Write("да, чет"); 
