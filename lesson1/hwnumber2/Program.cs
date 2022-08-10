d531fdb074a14e59d5ec4ce20182aa50cda10bf8
Ivan <markelovv1999@yandex.ru>
2022-08-10 12:48:06 +0300

    do hwnumber 2

--- lesson1/hwnumber2/Program.cs
+++ lesson1/hwnumber2/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число А сравнения:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число Б сравнения:");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > b) max = a;
+if (b > a) max = b;
+Console.Write("max = ");
+Console.Write(max);
+
+
+
