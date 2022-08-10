23ee921450c5b7071d887bbf78372b7b2bb907d9
Ivan <markelovv1999@yandex.ru>
2022-08-10 15:37:21 +0300

    add hwnumber6

---
+++ lesson1/hwnumber6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число для определения: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 != 0)
+    Console.Write("нет");
+else
+    Console.Write("да"); 
+
+
+
