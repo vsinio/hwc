e456698a7eda74e8499d1964cb03bed587ed86c9
Ivan <markelovv1999@yandex.ru>
2022-08-10 17:24:40 +0300

    make 2nd var hw2

---
+++ lesson1/hwnumber2.correct/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите число А сравнения:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число Б сравнения:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.Write(a);
+    Console.Write("min = ");
+    Console.Write(b);
+}
+else
+{
+    Console.Write("max = ");
+    Console.Write(b);
+    Console.Write("min = ");
+    Console.Write(a);
+}
+
+
