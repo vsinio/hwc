b8167d30c2dda702d613b0789814c9bf98105cb3
Ivan <markelovv1999@yandex.ru>
2022-08-11 17:01:09 +0300

    add hwnumber 13 with the greatest Taya

---
+++ lesson2/hwnumber15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a / 6 == 0 % a / 7 == 0)
+{
+    Console.Write(да, выходной);
+}
+else
+    Console.Write(нет, будний день);
