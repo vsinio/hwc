54a397260397a3e03bb6cc57d73e25c8bcef61a3
Ivan <markelovv1999@yandex.ru>
2022-08-11 16:55:52 +0300

    add hwnumber 13 with Asya

---
+++ lesson2/hwnumber13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a >= 100 & a < 1000)
+{
+    Console.Write(a % 10);
+}
+else if (a < 100)
+{
+    Console.Write("третьей цифры нет");
+}
+else if (a > 1000)
+{
+    while (a > 1000)
+    a = a / 10;
+    Console.Write(a % 10);        
+}
