4b9eb36b79b2761d4a06d69c7df14de968bd2920
Ivan <markelovv1999@yandex.ru>
2022-08-17 16:13:45 +0300

    watch lection with array and do test metods

--- lesson3/Program.cs
+++ lesson3/Program.cs
@@ -2,9 +2,9 @@
 {
     int length = collection.Length;
     int index = 0;
-    while(index<length)
+    while (index < length)
     {
-        collection[index] = new Random().Next(1,10);
+        collection[index] = new Random().Next(1, 10);
         index++;
     }
 }
@@ -13,7 +13,7 @@ void PrintArray(int[] col)
 {
     int count = col.Length;
     int position = 0;
-    while(position<count)
+    while (position < count)
     {
         Console.WriteLine(col[position]);
         position++;
@@ -25,20 +25,19 @@ int IndexOf(int[] collection, int find)
     int count = collection.Length;
     int index = 0;
     int position = -1;
-    while(index<count)
+    while (index < count)
     {
-        if(collection[index]==find)
+        if (collection[index] == find)
         {
-        position = index;
-        break;
+            position = index;
+            break;
         }
-    index++;
-
+        index++;
     }
     return position;
 }
 
-int [] array = new int [10];
+int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
