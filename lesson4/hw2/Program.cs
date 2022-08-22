c515a9b5ef48c3077d705069d591dac741c0b2af
Ivan <markelovv1999@yandex.ru>
2022-08-22 12:20:33 +0300

    do hw2 in lesson4

---
+++ lesson4/hw2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите матод, который принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+Console.Clear();
+Console.WriteLine("Введите число, чтобы получить сумму его цифр:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+int summ1(int arg)
+
+{
+
+    int lastint = 0;
+    int summa = 0;
+    
+    while(arg>=1)
+    {
+        lastint = arg % 10; 
+        arg = arg / 10; 
+        summa = summa + lastint;      
+    }
+    return summa;
+}
+int summfunc = summ1(a);
+Console.WriteLine($"Сумма цифр числа {a} = {summfunc}");
