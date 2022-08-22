b2d84e2e544ffa010b8b3c223e6d4cdeed284961
Ivan <markelovv1999@yandex.ru>
2022-08-22 09:28:47 +0300

    do hw1 in lesson4

---
+++ lesson4/hw1/Program.cs
@@ -0,0 +1,22 @@
+﻿///Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+///нельзя использовать pow
+///3, 5 -> 243 (3⁵)
+
+Console.Write("Введите 2 числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+
+int stepen(int a, int b)
+{   
+
+    int c = a ;
+
+    for(int count = 1; count < b; count++)
+    {
+        c = c * a;;
+    } 
+    return c;
+}
+Console.WriteLine(stepen(a,b));
