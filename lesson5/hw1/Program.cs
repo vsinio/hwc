c8a6cf7e997a8ea0bee530c79ab0392c87f7a3d2
Ivan <markelovv1999@yandex.ru>
2022-08-24 15:40:16 +0300

    do hw123 for lesson5

---
+++ lesson5/hw1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+Console.Clear();
+
+int[] mass = new int[4];
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    int pol = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        if (array[i] % 2 == 0)
+            pol++;
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write($"количество четных чисел в массиве => {pol}");
+}
+
+PrintArray(mass);
---
+++ lesson5/hw3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+int[] mass = new int[4];
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    int min = array[0];
+    int max = array[0];
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        if (array[i] > max)
+            max = array[i];
+        if (array[i] < max)
+            min = array[i];
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write($"разница => max({max})-min({min})= {max - min}");
+}
+
+PrintArray(mass);
