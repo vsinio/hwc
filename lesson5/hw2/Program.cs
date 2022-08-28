9c9d014be6dd155202861f6b23fb4351764be433
Ivan <markelovv1999@yandex.ru>
2022-08-28 14:50:04 +0300

    make some corrections

--- lesson5/hw2/Program.cs
+++ lesson5/hw2/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
 //Найдите сумму элементов, стоящих на нечётных позициях.
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
@@ -6,15 +6,16 @@
 Console.Clear();
 int[] mass = new int[4];
 
-void PrintArray(int []array)
+void PrintArray(int[] array)
 {
     int length = array.Length;
     int summ = 0;
 
-    for(int i=0; i < length;i++)
+    for (int i = 0; i < length; i++)
     {
         array[i] = new Random().Next(1, 10);
-        if(i == 1 || i%2==1) summ=summ + array[i];
+        if (i == 1 || i % 2 == 1)
+            summ = summ + array[i];
         Console.Write($"{array[i]} ");
     }
     Console.Write($"сумма нечетных элементов в массиве => {summ}");
