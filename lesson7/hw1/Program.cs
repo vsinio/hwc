bd23b7df5c0825ed2bc5ec53a2fc12ee4d9e6c41
Ivan <markelovv1999@yandex.ru>
2022-08-29 21:36:45 +0300

    do hw3

---
+++ lesson7/hw1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. 
+//m = 3, n = 4.
+
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+Console.Clear();
+
+double[,] matrix = new double[3, 4];
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"|{matr[i, j]}| "); //разделил палками для читабельности тк нет запятых + просьба, помогите со способом
+            // для добавления вещественных в массив без костылей, которые снизу ( new random / 10 )
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillArray(double[,] matr1)
+{
+    for (int i = 0; i < matr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr1.GetLength(1); j++)
+        {
+            matr1[i, j] = new Random().Next(-100, 100);
+            matr1[i,j] = matr1[i,j] / 10;
+        }
+    }
+}
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine("_______");
+PrintArray(matrix);
---
+++ lesson7/hw3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+
+
+int[,] matrix = new int[3, 4];
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"|{matr[i, j]}| "); //разделил палками для читабельности тк нет запятых + просьба, помогите со способом
+            // для добавления вещественных в массив без костылей, которые снизу ( new random / 10 )
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FillArray(int[,] matr1)
+{
+    for (int i = 0; i < matr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr1.GetLength(1); j++)
+        {
+            matr1[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void srarfm(int[,] matr1)
+{
+    
+    for (int i = 0; i < matr1.GetLength(0); i++)
+    {
+        double f = 0;
+        for (int j = 0; j < matr1.GetLength(1); j++)
+        {
+         f = matr1[i,j]+f;
+        }
+    Console.WriteLine($"Среднее арифм {i+1} столбца = {f/4} ");
+    }
+}
+
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine("---------------");
+PrintArray(matrix);
+srarfm(matrix);
