252bb9f4ad592bdcfb3037eabaf138b4ba8fe756
Ivan <markelovv1999@yandex.ru>
2022-08-29 21:41:46 +0300

    update hw1

--- lesson7/hw1/Program.cs
+++ lesson7/hw1/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. 
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 //m = 3, n = 4.
 
 //0,5 7 -2 -0,2
@@ -10,9 +10,9 @@ double[,] matrix = new double[3, 4];
 
 void PrintArray(double[,] matr)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)
+    for (int i = 0; i < matr.GetLength(0); i++) //getl(0) строка
     {
-        for (int j = 0; j < matr.GetLength(1); j++)
+        for (int j = 0; j < matr.GetLength(1); j++) // getl(1) столб
         {
             Console.Write($"|{matr[i, j]}| "); //разделил палками для читабельности тк нет запятых + просьба, помогите со способом
             // для добавления вещественных в массив без костылей, которые снизу ( new random / 10 )
@@ -21,7 +21,6 @@ void PrintArray(double[,] matr)
     }
 }
 
-
 void FillArray(double[,] matr1)
 {
     for (int i = 0; i < matr1.GetLength(0); i++)
@@ -29,7 +28,7 @@ void FillArray(double[,] matr1)
         for (int j = 0; j < matr1.GetLength(1); j++)
         {
             matr1[i, j] = new Random().Next(-100, 100);
-            matr1[i,j] = matr1[i,j] / 10;
+            matr1[i, j] = matr1[i, j] / 10;
         }
     }
 }
