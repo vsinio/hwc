64c060e0c72a7b1fbd9970a9e665974efdd427b0
Ivan <markelovv1999@yandex.ru>
2022-08-29 20:37:00 +0300

    make some task

---
+++ my training/task40/Program.cs
@@ -0,0 +1,47 @@
+﻿//40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+double[] mass = new double[8];
+
+void PrintArray(double []array)
+{
+    double length = array.Length;
+
+    for(int i=0; i < length;i++)
+    {
+        //array[i] = 0; // new Random().Next(1, 100); изначально array[i] = empty(пустой == 0)
+        Console.Write($"|{array[i]}| ");
+    }
+}
+void FillArray(double []array)
+{
+    int length = array.Length;
+
+    for(int i=0; i < length;i++)
+    {
+        array[i] = new Random().Next(10, 100);
+        array[i] = array[i] / 10;
+    }
+}
+
+void difference (double []array)
+{
+    int length = array.Length;
+    double min = array[0];
+    double max = array[0];
+    for(int i = 0; i<length; i++)
+    {
+        if(array[i]<min) 
+        {
+            min=array[i];
+        }
+        if(array[i]>max) 
+        {
+            max=array[i];
+        }
+    }
+    Console.WriteLine($"разница между наибольшим и наименьшим элементом = {max - min}");
+}
+
+FillArray(mass);
+Console.WriteLine();
+PrintArray(mass);
+difference(mass);
---
+++ my training/task41/Program.cs
@@ -0,0 +1,41 @@
+﻿// Выяснить являются ли три числа сторонами треугольника //решил сделать через массив
+Console.Clear();
+
+int[] mass = new int[3];
+
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"|{i+1} сторонa = {array[i]} ");
+    }
+}
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    Console.WriteLine("Введите 3 стороны треугольника для проверки:");
+    for (int i = 0; i < length; i++)
+    {
+        int a = Convert.ToInt32(Console.ReadLine()); 
+        array[i] = a;
+    }
+}
+
+void checktriangle(int [] array)
+{   int i = 0;
+    if(array[i]<array[i+1]+array[i+2] & array[i+1]<array[i]+array[i+2] & array[i+2]<array[i]+array[i+1]) Console.WriteLine("YES");
+
+
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
+
+FillArray(mass);
+PrintArray(mass);
+checktriangle(mass);
