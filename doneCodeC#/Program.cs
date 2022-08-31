0eecb2ec26cf398bcf687d3be1cd0ae4e8034e3c
Ivan <markelovv1999@yandex.ru>
2022-08-31 16:16:49 +0300

    add new code

--- doneCodeC#/Program.cs
+++ doneCodeC#/Program.cs
@@ -8,7 +8,6 @@ void PrintArray(int[] array)
 
     for (int i = 0; i < length; i++)
     {
-        //array[i] = 0; // new Random().Next(1, 100); изначально array[i] = empty(пустой == 0)
         Console.Write($"{array[i]} ");
     }
 }
@@ -85,3 +84,28 @@ PrintArray(matrix);
 FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
+
+
+
+// ввод чисел через " пробел " 
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+
+//создание массива с указанием его размера и вводом элементов через пробел
+
+Console.WriteLine("Введите целое значение — размер массива");
+int size = int.Parse(Console.ReadLine());               // спрашиваем длину массива у пользователя
+int[] array = new int[size];                            // объявляем массив на size элементов
+for (int i=0; i < array.Length; i++) 
+    {                  // проходим весь массив от 0 до size - 1
+    Console.WriteLine("Введите целое значение — элемент массива");
+    array[i] = int.Parse(Console.ReadLine());            // в i-й элемент запрашиваем значение
+    }
+
+
+
+//Перевернуть строку ! 
+string s = "Привет, мир!";           // есть строка
+char[] sReverse = s.ToCharArray();   // создаем массив char 
+Array.Reverse(sReverse);             // переворачиваем массив
+s = new string(sReverse);            // задаем строку из массива заново . ps. проще сразу вкидывать 
+                                     //в массив значения(если известно колво элементов)
