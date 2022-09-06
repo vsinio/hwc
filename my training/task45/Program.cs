bf636c201f8c3e9c7ce65d01904461fc523ffddc
Ivan <markelovv1999@yandex.ru>
2022-09-06 17:51:13 +0300

    do 45

---
+++ my training/task45/Program.cs
@@ -0,0 +1,26 @@
+﻿// 45. Показать числа Фибоначчи 1 1 2 3 5 8 13  ///1 + 1 =2 1 + 2 =3 2 + 3 =5
+
+Console.WriteLine("Введите число до которого отображать числа Фибоначчи:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+void Fibonacci(int a)
+{
+    int num = 1;
+    int num2 = 1;
+    int summ = 0;
+    Console.WriteLine(num);   
+    Console.WriteLine(num);
+    while(summ<a)
+    {
+        summ = num + num2; 
+        Console.WriteLine(summ); 
+        num = summ - num;
+        num2 = summ;
+
+
+    }
+}
+
+Fibonacci(a);
